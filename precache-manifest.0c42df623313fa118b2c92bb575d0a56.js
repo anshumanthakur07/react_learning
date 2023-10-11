870e8e78509f00158a2caa43a0802b4e4fcd9bcf
ANSHUMAN <anshumansinghchauhan786@gmail.com>
2023-10-11 23:27:23 +0530

    Updates

---
+++ precache-manifest.0c42df623313fa118b2c92bb575d0a56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd17b6940f414a5e32972b48a482358",
+    "url": "/react_learning/index.html"
+  },
+  {
+    "revision": "a721526e694b9f037ff1",
+    "url": "/react_learning/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bf5c34c705ad50e3c406",
+    "url": "/react_learning/static/js/2.af3fc02c.chunk.js"
+  },
+  {
+    "revision": "bd2db5e315108ebec595",
+    "url": "/react_learning/static/js/3.5b0ae9b5.chunk.js"
+  },
+  {
+    "revision": "a721526e694b9f037ff1",
+    "url": "/react_learning/static/js/main.7557c4e3.chunk.js"
+  },
+  {
+    "revision": "19ad5fa29d1e643f55c5",
+    "url": "/react_learning/static/js/runtime~main.22692b3b.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react_learning/static/media/logo.06e73328.svg"
+  }
+]);
